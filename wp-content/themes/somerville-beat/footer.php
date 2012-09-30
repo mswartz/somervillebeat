91366f53f3d1bd49cd36f53b45cc4c7337d5d0df
Michael Swartz <mswartz@gmail.com>
2012-09-30 15:52:38 -0400

    footer styling

--- wp-content/themes/somerville-beat/footer.php
+++ wp-content/themes/somerville-beat/footer.php
@@ -17,8 +17,6 @@
 	 */
 	get_sidebar( 'footer' );
 ?>
-			<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
 		</footer><!-- footer -->
 
 		</div><!-- site container -->
