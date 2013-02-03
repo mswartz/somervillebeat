f9960ebe0f048c832ec3ca9008566ea5a8b9a75d
Michael Swartz <mswartz@gmail.com>
2013-02-03 11:27:41 -0500

    adding directory

---
+++ wp-content/themes/somerville-beat/page-directory.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Template Name: Directory
+*/
+?>
+
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the wordpress construct of pages
+ * and that other 'pages' on your wordpress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Boilerplate
+ * @since Boilerplate 1.0
+ */
+
+get_header(); ?>
+
+	<section class="ui-block-1 page-content">
+
+		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<?php if ( is_front_page() ) { ?>
+							<h1><?php the_title(); ?></h1>
+						<?php } else { ?>	
+							<h1><?php the_title(); ?></h1>
+						<?php } ?>
+		
+							<div class="page-directory">
+								<?php the_content(); ?>
+								<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
+								<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
+							</div><!-- .page-directory -->
+						</article><!-- #post-## -->
+		<?php endwhile; ?>
+	</section>
+	<section id="sidebar" class="ui-block-2">
+	<?php get_sidebar(); ?>
+	</section>
+</section><!-- main -->
+<?php get_footer(); ?>
