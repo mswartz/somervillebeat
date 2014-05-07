30bd2d438cdc40cd9841542e49a7ec5a7ebbfcbe
Mike Swartz <mswartz@gmail.com>
2014-05-07 08:01:36 -0400

    installing Upbase

---
+++ wp-content/themes/somerville-beat/config.rb
@@ -0,0 +1,14 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+relative_assets = true
+
+line_comments = false
+output_style = :compressed
+# output_style = :compact :compressed :nested :expanded
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
